85082be1af756a73309a697f8accb909a694e7c4
Andres Montenegro <41881835+andressmm4@users.noreply.github.com>
2020-07-08 16:29:41 -0600

    Changed in form.php

--- form.php
+++ form.php
@@ -1,35 +1,60 @@
 <?php
-//Reseteamos variables a 0.
-$nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+$nombre = $_POST['nombre'];
+$email = $_POST['email']; 
+$mensaje = $_POST['message'];
+$receptor = "andiz132003";  
 
-if (isset($_POST['submit'])) {
-   //Obtenemos valores input formulario
-   $nombre = $_POST['name'];
-   $email = $_POST['email'];
-   $mensaje = $_POST['message'];
-   $para = 'andiz132003@gmail.com'; 
+$uniqueid= uniqid('np');
 
-   //Creamos cabecera.
-   $headers = 'From' . " " . $email . "\r\n";
-   $headers .= "Content-type: text/html; charset=utf-8";
+//indicamos las cabeceras del correo
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "From: $email \r\n";
+$headers .= "Subject: Test mail\r\n";
 
-   //Componemos cuerpo correo.
-   $msjCorreo = "Nombre: " . $nombre;
-   $msjCorreo .= "\r\n";
-   $msjCorreo .= "Email: " . $email;
-   $msjCorreo .= "\r\n";
-   $msjCorreo .= "Mensaje: " . $mensaje;
-   $msjCorreo .= "\r\n";
+//Es importante indicar que el Content-Type
+//es multipart/alternative de esta manera existirá un contenido alternativo
 
- if (mail($para, $subject, $msjCorreo, $headers)) {
-      echo "<script language='javascript'>
-         alert('Mensaje enviado, muchas gracias.');
-      </script>";
- } else {
-      echo "<script language='javascript'>
-         alert('fallado');
-      </script>";
- }
-}
+$headers .= "Content-Type: multipart/alternative;boundary=" . $uniqueid. "\r\n";
+   
+$message = "";
+    
+$message .= "\r\n\r\n--" . $uniqueid. "\r\n";
+$message .= "Content-type: text/plain;charset=utf-8\r\n\r\n";
+$message .= "E-mail en Texto Plano sin formato.";
+    
+
+// Este es el código que contiene la información y que además tiene formato
+
+$message .= "\r\n\r\n--" . $uniqueid. "\r\n";
+$message .= "Content-type: text/html;charset=utf-8\r\n\r\n";
+$message .= "<div style='background: #000000; border-radius: 2em; box-shadow: 5px 5px 10px #000000; color: #ffffff; height: auto; padding: 20px; position: relative; width: 90%;'";
+$message .= "<table border=0 width=auto>";
+$message .= "<tr><td><b>Nombre:</b> </td><td bgcolor=#F7D358><span style='font-size: 1.5em'>$nombre</span><br></td></tr>";
+$message .= "<tr><td><b>E-mail:</b> </td><td>$email<br></td></tr>";
+$message .= "<tr><td><b>Consulta:</b> </td><td>$mensaje<br></td></tr>";
+$message .= "</table>";
+$message .= "</div>";
+$message .= "<br>";
+    
+$message .= "\r\n\r\n--" . $uniqueid. "--";
+   
+//enviamos el correo...
+
+mail($receptor, 'Se ha recibido un nuevo mensaje de tu sitio web...', $message, $headers);
+  
+//doy gracias por el envío y redireccionamos al usuario a la página principal.
+?>
+<td align="center" height="400" width="55%">
+ <span style="color: #003366; font-family: mistral; font-size: 2.3em">Hemos recibido su información, muy pronto le responderemos.</span>
+</td>
+
+  <script type="text/javascript">
+   function redireccionar(){
+    location.href="index.php";
+   }
+   setTimeout ("redireccionar()", 3000);
+  </script>
+
+<?
 
 ?>
